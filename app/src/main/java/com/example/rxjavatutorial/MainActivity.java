915fe294b5ec44eabbd39f6df5731bb4498ec086
khishh <khiraishi0319@gmail.com>
2020-10-19 23:16:10 +0900

    basic RxBinding

--- app/src/main/java/com/example/rxjavatutorial/MainActivity.java
+++ app/src/main/java/com/example/rxjavatutorial/MainActivity.java
@@ -3,9 +3,20 @@ package com.example.rxjavatutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.jakewharton.rxbinding4.view.RxView;
+import com.jakewharton.rxbinding4.widget.RxTextView;
+
+
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +33,58 @@ public class MainActivity extends AppCompatActivity {
         viewText = findViewById(R.id.tvInput);
         clearButton = findViewById(R.id.btnClear);
 
+        // without RxBinding starts
+
+        /*
+        inputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                viewText.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputText.setText("");
+                viewText.setText("");
+            }
+        });
+
+        // without RxBinding ends
+
+         */
+
+        // With RxJava, we can convert Android view events into Observable
+        // Once convert a view event into an Observable, it will emit its UI events as streams of data we can subscribe
+
+        Disposable textChangeDisposable = RxTextView.textChanges(inputText)
+                .subscribe(new Consumer<CharSequence>() {
+                    @Override
+                    public void accept(CharSequence charSequence) throws Throwable {
+                        viewText.setText(charSequence);
+                    }
+                });
+
+        Disposable buttonClickDisposable = RxView.clicks(clearButton)
+                .subscribe(new Consumer<Unit>() {
+                    @Override
+                    public void accept(Unit unit) throws Throwable {
+                        inputText.setText("");
+                        viewText.setText("");
+                    }
+                });
+
     }
+    
 }
